e54ef1525158500689e1202f2c36d831c42cd80b
SAIKAT <mohammadislam.snhu@gmail.com>
2023-03-15 08:00:33 +0400

    feat: add navigation bar with responsive hamburger menu

---
+++ js/index.js
@@ -0,0 +1,27 @@
+const openMenu = getElement('.architect--header-open-menu');
+const closeMenu = getElement('.architect--header-close-menu');
+const mainMenu = getElement('.architect--header-main-menu')
+
+console.log(openMenu,closeMenu, mainMenu)
+
+addListener(openMenu, 'click', function() {
+    console.log("clicke")
+    openMenu.style.display = 'none';
+    closeMenu.style.display = 'block';
+    mainMenu.style.top = '0';
+});
+
+addListener(closeMenu, 'click', function() {
+    console.log("clicke")
+    openMenu.style.display = 'block';
+    closeMenu.style.display = 'none';
+    mainMenu.style.top = '-100%';
+});
+
+function getElement(id) {
+    return document.querySelector(id);
+}
+
+function addListener(element, eventType, callBack) {
+    element.addEventListener(eventType, callBack);
+}
